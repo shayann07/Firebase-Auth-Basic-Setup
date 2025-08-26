bf0700b96694a98c7a21036f4a1c26cbf74cb656
shayann07 <shayankhan17353@gmail.com>
2025-08-26 22:01:36 +0500

    Firebase Auth Setup!

--- app/src/main/java/com/shayan/firebaseauthsetup/LoginActivity.kt
+++ app/src/main/java/com/shayan/firebaseauthsetup/LoginActivity.kt
@@ -1,14 +1,58 @@
 package com.shayan.firebaseauthsetup
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.shayan.firebaseauthsetup.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnLogin.setOnClickListener {
+
+            login()
+        }
+        binding.btnGoRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        auth.currentUser?.let {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+            Toast.makeText(this, auth.currentUser?.uid, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun login() {
+        val email = binding.etEmail.text.toString()
+        val password = binding.etPassword.text.toString()
+
+        if (email.isEmpty() && password.isEmpty()) {
+            Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            } else {
+                Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
